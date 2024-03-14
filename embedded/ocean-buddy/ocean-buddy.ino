e6b6155278c2d3e607dc0044070f307223908d49
EmilianaAP <EmilianaAP>
2024-03-14 23:30:54 +0200

    Added temperature sensor. All sensors are working properly :)

--- embedded/ocean-buddy/ocean-buddy.ino
+++ embedded/ocean-buddy/ocean-buddy.ino
@@ -1,13 +1,18 @@
 #include <WiFiManager.h>
+#include "DHT.h"
+
 
 const int soundLevelPin = 32;
-const int photoResistorPin = 4; //Tolerance +-0.1%
+const int photoResistorPin = 4; // Tolerance +-0.1%
 const int salinityLevelPin = 13;
+const int temperaturePin = 19; // +-0.5C
 
-void  setup() {
+DHT dht(temperaturePin, DHT22);
+
+void setup() {
   Serial.begin(9600);
-
-  //Wi-Fi connection
+  dht.begin();
+  // Wi-Fi connection
   /*WiFiManager wm;
   bool res = wm.autoConnect("AutoConnectAP", "password");
   if (!res) {
@@ -21,13 +26,16 @@ void loop() {
   int photoResistorValue = analogRead(photoResistorPin);
   int soundLevelValue = analogRead(soundLevelPin);
   int salinityLevelValue = analogRead(salinityLevelPin);
+  float temperatureValue = dht.readTemperature(false);
 
   Serial.print("Light level: ");
   Serial.println(photoResistorValue);
   Serial.print("Sound level: ");
-  Serial.println(soundLevelValue); 
+  Serial.println(soundLevelValue);
   Serial.print("Salinity level: ");
   Serial.println(salinityLevelValue);
+  Serial.print("Temperature: ");
+  Serial.println(temperatureValue);
 
-  delay(500);
+  delay(10000);
 }
