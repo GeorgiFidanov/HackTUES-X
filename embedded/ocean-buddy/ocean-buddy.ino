18927463cc23d44811c1dbf808d1fbe666eedf71
EmilianaAP <EmilianaAP>
2024-03-14 12:20:59 +0200

    Added light lever recognition in the embedded part, which works with photoresistor

--- embedded/ocean-buddy/ocean-buddy.ino
+++ embedded/ocean-buddy/ocean-buddy.ino
@@ -1,11 +1,16 @@
 const int soundPin = 32;
+const int photoResistorPin = 4;
 
 void  setup() {
   Serial.begin(9600);
 }
 
 void loop() {
+  int photoResistorValue = analogRead(photoResistorPin);
   int soundValue = analogRead(soundPin); // Read the analog value from the sound sensor
+  Serial.print("Light level:");
+  Serial.println(photoResistorValue);
+  Serial.print("Sound level:");
   Serial.println(soundValue); // Print the value to the serial monitor
   delay(1000); // Delay for a second before reading again
 }
