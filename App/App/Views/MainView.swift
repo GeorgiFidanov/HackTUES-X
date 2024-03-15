6867d44b4a671b87ce5dfcb26eae594bf95cf6c7
Petya Damyanova <petyadamyanova@gmail.com>
2024-03-15 08:41:59 +0200

    adding IOS app

---
+++ App/App/Views/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  App
+//
+//  Created by Andrea Petrenko on 14.03.24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Welcome!")
+    }
+}
+
+#Preview {
+    MainView()
+}
