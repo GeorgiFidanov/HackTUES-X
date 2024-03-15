9f847fe90d854fbeff543d3048f838889559f892
PavelVoykov <pavelepavlov@abv.bg>
2024-03-15 16:10:15 +0200

    Embedded comunication with backend endpoint and sensors

--- embedded/ocean-buddy/ocean-buddy.ino
+++ embedded/ocean-buddy/ocean-buddy.ino
@@ -1,25 +1,29 @@
 #include <WiFiManager.h>
+#include <HTTPClient.h>
+#include <stdio.h>
 #include "DHT.h"
-
+#define V_REF 3.3
+#define ID 1
+#include <string>
 
 const int soundLevelPin = 32;
 const int photoResistorPin = 4; // Tolerance +-0.1%
 const int salinityLevelPin = 13;
 const int temperaturePin = 19; // +-0.5C
+const char* serverName = "http://192.168.166.172:8000/api/data";
 
 DHT dht(temperaturePin, DHT22);
 
 void setup() {
   Serial.begin(9600);
   dht.begin();
-  // Wi-Fi connection
-  /*WiFiManager wm;
+  WiFiManager wm;
   bool res = wm.autoConnect("AutoConnectAP", "password");
   if (!res) {
     Serial.println("Failed to connect");
   } else {
     Serial.println("connected...yeey :)");
-  }*/
+  }
 }
 
 void loop() {
@@ -27,16 +31,40 @@ void loop() {
   int soundLevelValue = analogRead(soundLevelPin);
   int salinityLevelValue = analogRead(salinityLevelPin);
   float temperatureValue = dht.readTemperature(false);
+  float voltagePhoto = (photoResistorValue / 4095.0) * V_REF;
+  float voltageSound = (soundLevelValue / 4095.0) * V_REF;
+  float voltageSalt = (salinityLevelValue / 4095.0) * V_REF;
+  HTTPClient http;
+  http.begin(serverName);
+  http.addHeader("Content-Type", "application/json");
+  std::string httpRequestData = "{\"device_id\":"+ std::to_string(ID) +",\"temperature\":27,\"noise\":"+ std::to_string(voltageSound) +",\"salinity\":"+ std::to_string(voltageSalt) +",\"murkiness\":"+ std::to_string(voltagePhoto) +"}";
+  int httpResponseCode = http.POST(httpRequestData.c_str());
+  Serial.print("HTTP Response code: ");
+  Serial.println(httpResponseCode);
+  http.end();
 
-  Serial.print("Light level: ");
+  Serial.println(httpRequestData.c_str());
+  Serial.print("Light level - ");
   Serial.println(photoResistorValue);
-  Serial.print("Sound level: ");
+  Serial.print("Light level volts - ");
+  Serial.println(voltagePhoto);
+  Serial.print("Sound level - ");
   Serial.println(soundLevelValue);
-  Serial.print("Salinity level: ");
+  Serial.print("Sound level volts - ");
+  Serial.println(voltageSound);
+
+  Serial.print("Salinity level - ");
   Serial.println(salinityLevelValue);
+  Serial.print("Salinity level volts - ");
+  Serial.println(voltageSalt);
   Serial.print("Temperature: ");
   Serial.println(temperatureValue);
-  //check
 
   delay(10000);
+  Serial.println();
+  Serial.println();
+  Serial.println();
+  Serial.println();
+  Serial.println();
+  Serial.println();
 }
