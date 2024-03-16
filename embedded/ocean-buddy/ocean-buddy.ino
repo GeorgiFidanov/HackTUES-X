f4eebbce5b5ecf57b0db1c11a4d1ca979cab55f5
PavelVoykov <71674555+PavelVoykov@users.noreply.github.com>
2024-03-16 05:51:13 +0200

    Comments

--- embedded/ocean-buddy/ocean-buddy.ino
+++ embedded/ocean-buddy/ocean-buddy.ino
@@ -1,44 +1,62 @@
+//Include the used libraries
 #include <WiFiManager.h>
 #include <HTTPClient.h>
 #include <Ticker.h>
 #include <stdio.h>
 #include "DHT.h"
-#define V_REF 3.3
-#define ID 1
 #include <string>
 
+//Define constants
+#define V_REF 3.3
+#define ID 1
+
+//Define pins used by sensors
 const int soundLevelPin = 32;
 const int photoResistorPin = 33; // Tolerance +-0.1%
 const int salinityLevelPin = 35;
 const int temperaturePin = 19; // +-0.5C
+
+//Set the address on which the data is sent to the backend
 const char* serverName = "http://192.168.166.172:8000/api/data";
 
 DHT dht(temperaturePin, DHT22);
 
+//Define the callback function to be executed every 10 minutes
 void sendData() {
-  int photoResistorValue = analogRead(photoResistorPin);
-  int soundLevelValue = analogRead(soundLevelPin);
-  int salinityLevelValue = analogRead(salinityLevelPin);
-  float temperatureValue = dht.readTemperature(false);
+  
+  //Read data from the sensors
+  int photoResistorValue = analogRead(photoResistorPin);//Murkiness
+  int soundLevelValue = analogRead(soundLevelPin);//Noise
+  int salinityLevelValue = analogRead(salinityLevelPin);//Salinity
+  float temperatureValue = dht.readTemperature(false);//Temperature
   float voltagePhoto = (photoResistorValue / 4095.0) * V_REF;
   float voltageSound = (soundLevelValue / 4095.0) * V_REF;
   float voltageSalt = (salinityLevelValue / 4095.0) * V_REF;
+
+  //Create HTTP POST request
   HTTPClient http;
   http.begin(serverName);
+  
+  //Fill the request with the mesured data
   http.addHeader("Content-Type", "application/json");
   std::string httpRequestData = "[{\"device_id\":"+ std::to_string(ID) +",\"temperature\":"+ std::to_string(temperatureValue) +",\"noise\":"+ std::to_string(voltageSound) +",\"salinity\":"+ std::to_string(voltageSalt) +",\"murkiness\":"+ std::to_string(voltagePhoto) +"}]";
+
+  //Send the request and display the response
   int httpResponseCode = http.POST(httpRequestData.c_str());
   Serial.print("HTTP Response code: ");
   Serial.println(httpResponseCode);
+  
+  //Handle the end of the connection with the backend
   http.end();
 }
-
+//Define timer
 Ticker timer;
 
 void setup() {
   Serial.begin(9600);
   dht.begin();
-
+  
+  //Setup WiFi connection at the beginning of the execution and display result in the console
   WiFiManager wm;
   bool res = wm.autoConnect("AutoConnectAP", "password");
   if (!res) {
@@ -46,9 +64,11 @@ void setup() {
   } else {
     Serial.println("connected...yeey :)");
   }
-
+  
+  //Setup the timer to call sendData() every 10 minutes
   timer.attach(600.0, sendData);
 }
+
 void loop() {
 
 }
